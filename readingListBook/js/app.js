cab2e320fd8d12830287dd4cb46edc2103ad06d7
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2016-06-05 22:22:24 -0300

    finalização de mais um projeto desenvolvido em angular. YEAH!

--- readingListBook/js/app.js
+++ readingListBook/js/app.js
@@ -1,14 +1,13 @@
-'use strict';
-
 (function() {
+	'use strict';
 
 	/*Declaração do módulo da aplicação */
 	angular.module('readingListBook', [])
 
 	/* Controlador responsável pela leitura de dados dos Livros */
 	.controller('ReadingListController', function() {
-		this.genres = genres;
 		this.books = books;
+		this.genres = genres;		
 	})
 
 	/* Diretiva para os Gêneros dos Livros */
@@ -60,7 +59,7 @@
 	/* Declaração de dados de exemplo a serem inseridos no projeto */
 	var genres = ['fantasia', 'ficção', 'fábulas', 'infantis', 'romance', 'folclore', 'horror', 'lenda', 'mistério', 'mitologia', 'não-ficção', 'poesia', 'romance'];
 
-	/* Dados de Livros */
+	/* Dados dos Livros */
 	var books = [
 		{
 			title: 'A Game of Thrones: A Song of Ice and Fire',
@@ -68,28 +67,28 @@
 			isbn: '0553593714',
 			review: 'A saga mais criativa e divertida dos últimos tempos - garante uma grande introdução ao mundo da fantasia. Eu amei esse livro!',
 			rating: 4,
-			genres: { 'não-ficção': true, fantasy: true }
+			genres: { 'não-ficção': true, fantasia: true }
 		}, {
 			title: 'HTML for Babies',
 			author: 'John C Vanden-Heuvek Sr',
 			isbn: '0615487661',
 			review: 'Nunca é cedo demais para aprender as novas normas compatíveis! Eu aprendi a realizar o meu primeiro mark-up em menos de uma hora!',
 			rating: 5,
-			genres: { fiction: true }	
+			genres: { ficção: true }	
 		}, {
 			title: 'A is for Array',
 			author: 'Brandon J Hansen',
 			isbn: '1489522212',
 			review: 'O livro: A is for Array é literalmente o ABC para futuros programadores. Cheio de ilustrações divertidas e simples exemplos de casos reais.',
 			rating: 4,
-			genres: { fiction: true }
+			genres: { ficção: true }
 		}, {
 			title: 'The Dragon Reborn',
 			author: 'Robert Jordan',
 			isbn: '0812513711',
 			review: 'A Roda acompanha como a roda quer e nós somos apenas o segmento do padrão.',
 			rating: 4,
-			genres: { 'não-ficção': true, fantasy: true }
+			genres: { 'não-ficção': true, fantasia: true }
 		}
 	];
 })();
