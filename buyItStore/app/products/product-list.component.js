87a02df6897bb350a06ce1ca8946fb70bd0b465a
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2016-06-19 02:09:26 -0300

    criação de uma pipe customizada para realizar busca de produtos de acordo com que está sendo digitado no input

--- buyItStore/app/products/product-list.component.js
+++ buyItStore/app/products/product-list.component.js
@@ -1,5 +1,5 @@
 /* Arquivo Componente 'ts' - Lista de Produtos */
-System.register(['angular2/core'], function(exports_1, context_1) {
+System.register(['angular2/core', './product-filter.pipe'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -11,12 +11,15 @@ System.register(['angular2/core'], function(exports_1, context_1) {
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1;
+    var core_1, product_filter_pipe_1;
     var ProductListComponent;
     return {
         setters:[
             function (core_1_1) {
                 core_1 = core_1_1;
+            },
+            function (product_filter_pipe_1_1) {
+                product_filter_pipe_1 = product_filter_pipe_1_1;
             }],
         execute: function() {
             ProductListComponent = (function () {
@@ -59,7 +62,8 @@ System.register(['angular2/core'], function(exports_1, context_1) {
                     core_1.Component({
                         selector: 'pm-products',
                         templateUrl: 'app/products/product-list.component.html',
-                        styleUrls: ['app/products/product-list.component.css']
+                        styleUrls: ['app/products/product-list.component.css'],
+                        pipes: [product_filter_pipe_1.ProductFilterPipe]
                     }), 
                     __metadata('design:paramtypes', [])
                 ], ProductListComponent);
--- buyItStore/app/products/product-list.component.ts
+++ buyItStore/app/products/product-list.component.ts
@@ -2,11 +2,13 @@
 
 import { Component, OnInit } from 'angular2/core';
 import { IProduct } from './product';
+import { ProductFilterPipe } from './product-filter.pipe'
 
 @Component ({
     selector: 'pm-products',
     templateUrl: 'app/products/product-list.component.html',
-    styleUrls:['app/products/product-list.component.css']
+    styleUrls:['app/products/product-list.component.css'],
+    pipes: [ProductFilterPipe]
 })
 
 /* Classe em .ts que corresponde aos atributos relacionados aos Produtos */
