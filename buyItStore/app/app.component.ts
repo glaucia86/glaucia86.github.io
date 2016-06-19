b22e93cf75b57b9f2b7989b9f24e1fb62662401e
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2016-06-18 23:13:05 -0300

    criação de um novo component para listar os produtos no projeto

--- buyItStore/app/app.component.ts
+++ buyItStore/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from 'angular2/core';
+import { ProductListComponent } from './products/product-list.component';
 
 @Component({
     selector: 'pm-app',
     template: `
     <div><h1>{{pageTitle}}</h1>
-        <div>Meu primeiro Componente</div>
+        <pm-products></pm-products>
     </div>
-    `
+    `,
+    directives: [ProductListComponent]
 })
 
 export class AppComponent {
