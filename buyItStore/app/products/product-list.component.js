609a4de851f54df29a65d8b3d637d826ca4c5ecb
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2016-06-19 00:53:15 -0300

    alteração das imagens no arquivo 'product-list.component.ts'

--- buyItStore/app/products/product-list.component.js
+++ buyItStore/app/products/product-list.component.js
@@ -22,29 +22,29 @@ System.register(['angular2/core'], function(exports_1, context_1) {
             ProductListComponent = (function () {
                 function ProductListComponent() {
                     this.pageTitle = 'Lista de Produtos';
-                    this.imageWidth = 50;
-                    this.imageMargin = 2;
+                    this.imageWidth = 150;
+                    this.imageMargin = 15;
                     this.showImage = false;
-                    this.listFilter = 'cart'; /* definimos aqui uma string de modelo */
+                    this.listFilter = 'produto'; /* definimos aqui uma string de modelo */
                     this.products = [
                         {
                             "productId": 1,
-                            "productName": "Leaf Rake",
+                            "productName": "iPhone 5S 16GB Prata Tela 4 IOS 8 4G Câmera de 8MP",
                             "productCode": "GDN-0011",
-                            "releaseDate": "March 19, 2016",
-                            "description": "Leaf rake with 48-inch wooden handle.",
-                            "price": 19.95,
-                            "starRating": 3.2,
-                            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+                            "releaseDate": "19 de Março de 2016",
+                            "description": "Com um design fino e elegante, o iPhone 5S traz recursos avançados por um preço excelente, apresentando tela Retina de 4 polegadas, chip A7 com arquitetura de 64 bits, sensor de impressão digital Touch ID, câmera iSight de 8MP, câmera FaceTime HD, 4G LTE e Wi-Fi*, iOS 8 e iCloud.",
+                            "price": 1.899,
+                            "starRating": 5,
+                            "imageUrl": "https://static.wmobjects.com.br/hotsite/sku-content/apple/418764/images/418764-apple-iphone-5s-16gb-prata-espacial-desbloqueado-ios7-product-image01.png"
                         }, {
                             "productId": 2,
-                            "productName": "Garden Cart",
+                            "productName": "Samsung Galaxy Tab A P550",
                             "productCode": "GDN-0023",
-                            "releaseDate": "March 18, 2016",
-                            "description": "15 gallon capacity rolling garden cart",
-                            "price": 32.99,
+                            "releaseDate": "19 de Junho de 2016",
+                            "description": "O Samsung Galaxy Tab A P550 oferece uma experiência tecnológica incrível e altamente funcional. Com sistema operacional Android 5.0, possui tela de 9.7 polegadas para a exibição de diversos conteúdos e também conta com processador Quad Core 1.2GHz para uma navegação mais rápida e fluida.",
+                            "price": 1.500,
                             "starRating": 4.2,
-                            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
+                            "imageUrl": "http://isuba1-a.akamaihd.net/produtos/01/00/item/122899/1/122899106_1GG.png"
                         }
                     ];
                 }
--- buyItStore/app/products/product-list.component.ts
+++ buyItStore/app/products/product-list.component.ts
@@ -10,29 +10,29 @@ import { Component } from 'angular2/core';
 /* Classe em .ts que corresponde aos atributos relacionados aos Produtos */
 export class ProductListComponent {
     pageTitle: string = 'Lista de Produtos';
-    imageWidth: number = 50;
-    imageMargin: number = 2;
+    imageWidth: number = 150;
+    imageMargin: number = 15;
     showImage: boolean = false;
-    listFilter: string = 'cart'; /* definimos aqui uma string de modelo */
+    listFilter: string = 'produto'; /* definimos aqui uma string de modelo */
     products: any[] = [
         {
             "productId": 1,
-            "productName": "Leaf Rake",
+            "productName": "iPhone 5S 16GB Prata Tela 4 IOS 8 4G Câmera de 8MP",
             "productCode": "GDN-0011",
-            "releaseDate": "March 19, 2016",
-            "description": "Leaf rake with 48-inch wooden handle.",
-            "price": 19.95,
-            "starRating": 3.2,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+            "releaseDate": "19 de Março de 2016",
+            "description": "Com um design fino e elegante, o iPhone 5S traz recursos avançados por um preço excelente, apresentando tela Retina de 4 polegadas, chip A7 com arquitetura de 64 bits, sensor de impressão digital Touch ID, câmera iSight de 8MP, câmera FaceTime HD, 4G LTE e Wi-Fi*, iOS 8 e iCloud.",
+            "price": 1.899,
+            "starRating": 5,
+            "imageUrl": "https://static.wmobjects.com.br/hotsite/sku-content/apple/418764/images/418764-apple-iphone-5s-16gb-prata-espacial-desbloqueado-ios7-product-image01.png"
         },{
             "productId": 2,
-            "productName": "Garden Cart",
+            "productName": "Samsung Galaxy Tab A P550",
             "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2016",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
+            "releaseDate": "19 de Junho de 2016",
+            "description": "O Samsung Galaxy Tab A P550 oferece uma experiência tecnológica incrível e altamente funcional. Com sistema operacional Android 5.0, possui tela de 9.7 polegadas para a exibição de diversos conteúdos e também conta com processador Quad Core 1.2GHz para uma navegação mais rápida e fluida.",
+            "price": 1.500,
             "starRating": 4.2,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
+            "imageUrl": "http://isuba1-a.akamaihd.net/produtos/01/00/item/122899/1/122899106_1GG.png"
         }   
     ];
 
