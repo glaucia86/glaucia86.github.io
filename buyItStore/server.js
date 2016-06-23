e699bc91b91946805b4805493ed1498ac96e76a9
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2016-06-23 14:35:37 -0300

    pequenos detalhes no projeto

--- buyItStore/server.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.use(express.static(__dirname + "/app"));
-app.listen(process.env.PORT || 3000);
-console.log("Server up on port 3000");
