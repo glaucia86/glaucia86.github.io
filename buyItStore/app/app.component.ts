c012f66021735672372c34f1819cb174bc92e931
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2016-06-23 22:38:34 -0300

    alterações no visual do projeto

--- buyItStore/app/app.component.ts
+++ buyItStore/app/app.component.ts
@@ -26,6 +26,31 @@ import { ProductDetailComponent } from './products/product-detail.component';
             <router-outlet></router-outlet>
         </div>
      </div>
+     <footer>
+      <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-6 col-lg-8">
+                <h3 class="white-text">Informações da Empresa:</h3>
+                <p class="white-text">We are a team of college students working on this project like it's our full time job.</p>
+            </div>
+            <div class="col-xs-6 col-lg-4">
+                <h3 class="white-text">Gostou? Entre em Contato!</h3>
+                <ul class="social">
+                    <li><a href="https://br.linkedin.com/in/glaucialemos"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a></li>
+                    <li><a href="https://github.com/glaucia86"><i class="fa fa-github-alt" aria-hidden="true"></i></a></li>
+                    <li><a href="https://code4coders.wordpress.com/"><i class="fa fa-wordpress" aria-hidden="true"></i></a></li>
+                    <li><a href="https://twitter.com/glaucia_lemos86"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                    <li><a href="https://goo.gl/XpCF3q"><i class="fa fa-youtube-square" aria-hidden="true"></i></a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="footer.footer-copyright">
+            <div class="container">
+            Developed by <a href="www.code4coders.wordpress.com">Glaucia Lemos</a>
+            </div>
+        </div>
+      </div>
+    </footer>
     ` ,
     directives: [ROUTER_DIRECTIVES],
     providers:  [ProductService,
