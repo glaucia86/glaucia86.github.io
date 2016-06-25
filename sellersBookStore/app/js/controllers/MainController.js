3f7181be56d7a9d7b2147b7808570f7cc56d72d0
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2016-06-25 00:02:41 -0300

    package.json index.html MainController.js

--- sellersBookStore/app/js/controllers/MainController.js
+++ sellersBookStore/app/js/controllers/MainController.js
@@ -8,28 +8,37 @@ app.controller('MainController', ['$scope', function($scope) {
    	  price: 19,
       pubDate: new Date('2014', '03', '08'),
       cover: 'img/the-book-of-trees.jpg',
-      likes: 0
+      likes: 0,
+      dislikes: 0
     },
     {
       name: 'Program or be Programmed',
       price: 8,
       pubDate: new Date('2016', '08', '01'),
       cover: 'img/program-or-be-programmed.jpg',
-      likes: 0
+      likes: 0,
+      dislikes: 0
     },
     {
       name: 'Star Wars - Character Encyclopedia (Updated and Expanded)',
       price: 11.59,
       pubDate: new Date('2016', '05', '01'),
       cover: 'img/star-wars-encyclopedia.jpg',
-      likes: 0
+      likes: 0,
+      dislikes: 0
     },
     {
       name: 'The Science of Good Cooking (Illustrated Cookbooks)',
       price: 21.81,
       pubDate: new Date('2012', '10', '01'),
       cover: 'img/the-science-of-good-cooking.jpg',
-      likes: 0
+      likes: 0,
+      dislikes: 0
     }
   ];
+
+  /* Função para poder adicionar os likes dos produtos */
+   $scope.plusOne = function(index) {
+    $scope.products[index].likes += 1;
+  };
 }]);
