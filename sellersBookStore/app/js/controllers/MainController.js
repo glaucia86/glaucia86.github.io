259d5f4986a135c8e240a131e8deab6661530fc1
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2016-06-24 23:35:12 -0300

    Adicionando novos livros no projeto

--- sellersBookStore/app/js/controllers/MainController.js
+++ sellersBookStore/app/js/controllers/MainController.js
@@ -7,13 +7,29 @@ app.controller('MainController', ['$scope', function($scope) {
       name: 'The Book of Trees',
    	  price: 19,
       pubDate: new Date('2014', '03', '08'),
-      cover: 'img/the-book-of-trees.jpg'
+      cover: 'img/the-book-of-trees.jpg',
+      likes: 0
     },
     {
       name: 'Program or be Programmed',
       price: 8,
       pubDate: new Date('2016', '08', '01'),
-      cover: 'img/program-or-be-programmed.jpg'
+      cover: 'img/program-or-be-programmed.jpg',
+      likes: 0
+    },
+    {
+      name: 'Star Wars - Character Encyclopedia (Updated and Expanded)',
+      price: 11.59,
+      pubDate: new Date('2016', '05', '01'),
+      cover: 'img/star-wars-encyclopedia.jpg',
+      likes: 0
+    },
+    {
+      name: 'The Science of Good Cooking (Illustrated Cookbooks)',
+      price: 21.81,
+      pubDate: new Date('2012', '10', '01'),
+      cover: 'img/the-science-of-good-cooking.jpg',
+      likes: 0
     }
   ];
 }]);
